56453d5d214d84f33ce951fe105464840cd9be63
Abdussamet ERSOYLU <abdussametersoylu@gmail.com>
2024-10-23 05:53:31 +0300

    introducing errors and constructing with params.

--- MemManage.hpp
+++ MemManage.hpp
@@ -3,38 +3,99 @@
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+#include <exception>
+#include <iostream>
 #include <type_traits>
+#include <utility>
 #include <vector>
 #include <algorithm>
 #include <string>
 
-
+/* 
+    This namespace provides passive automatic memory management
+    and memory safety (not security) features for heap allocated
+    objects.
+*/
 namespace AutomaticMemory {
     /* 
         This class is an interface class for Pointer types.
         Essentially, this provides a common ground for heap::pointer
-        and stack::pointer (not implemented, waits further implementation).
+        and stack::pointer (if stack gets implemented).
     */
+    namespace Errors {
+        class base_error {
+            public: 
+            base_error() = default;
+            base_error(base_error const&) = delete; 
+            base_error(base_error&& other) : message(other.message), error_code(other.error_code), exit(other.exit) {
+                other.dont_exit();
+            }
+            base_error(std::string const& error_message, int error_code = -1) : message(error_message), error_code(error_code), exit(true) { 
+                std::cerr << "Error: \"" << message << "\" at line " << __LINE__ << " in file \"" << __FILE_NAME__ << "\"." << std::endl; 
+            }
+            ~base_error() {
+                if (exit) {
+                    std::exit(error_code);
+                }
+            }
+            base_error& operator=(base_error&& other) {
+                message = std::move(other.message);
+                error_code = std::move(other.error_code);
+                other.dont_exit();
+                return *this;
+            }
+            void dont_exit() const {
+                exit = false; 
+            }
+            std::string what() const {
+                return message; 
+            }
+            protected: 
+            std::string message{};
+            int error_code = -1; 
+            mutable bool exit = false;
+        };
+
+        class BadConstruct : public base_error {
+            public: 
+            BadConstruct() : base_error("An error occurred while constructing object.", -2) {}
+            BadConstruct(std::string const& message) : base_error(message, -2) {}
+            BadConstruct(BadConstruct&& other) : base_error(other.message, other.error_code) { other.dont_exit(); } 
+        };
+
+        class IndexOutOfBounds : public base_error {
+            public: 
+            IndexOutOfBounds() : base_error("Index out of bounds", -3) {}
+            IndexOutOfBounds(std::string const& message) : base_error(message, -3) {}
+            IndexOutOfBounds(IndexOutOfBounds&& other) : base_error(other.message, other.error_code) { other.dont_exit(); }
+        };
+
+        
+    }
+
     template<typename T_, typename D_>
     class base_pointer {
         public: 
-        // Creates a pointer from value. 
-        // Type must be copy constructable.
+        /* 
+            This structure SHOULD NOT be copied. Copying heap allocated pointers around
+            is not a good idea if you ARE NOT counting references to free up the memory.
+            It can cause dangling pointers OR free after use. But also not being able to
+            pass memory that is allocated is not a good idea. So we transfer the pointer 
+            instead. Please check Heap::Pointer::Pointer(Pointer&& other) to see move 
+            (transfer) operations.   
+        */
         base_pointer(base_pointer const&) = delete; 
-        // RAII.
+        /* 
+            Resource Acquisition is Initialization. 
+            RAII Ensures that the object is VALID at any given time of it's lifetime. IF 
+            object is alive, and still accessable, it ensures the data or space that object
+            uses is also available. And also ensures the memory is released after it's use
+            is complete. 
+        */
         ~base_pointer() {
             free(); 
         }
 
-        /* 
-            Freeing is handled by the derived class. Free should never be used by hand. 
-        */
-        void free() {
-            if (freed) return; 
-            static_cast<D_*>(this)->free_impl();
-            freed = true; 
-        }
-        
         // Arrow operator for pointer access.
         T_ * operator->() {
             return m_Ptr; 
@@ -50,9 +111,18 @@ namespace AutomaticMemory {
         base_pointer(T_*&& pointer, bool&& freed) : m_Ptr(pointer), freed(freed) {}
         bool freed = false; 
         T_ * m_Ptr;
+        private:
+        /* 
+            Freeing is handled by the derived class. Free should never be used by hand. 
+        */
+        void free() {
+            if (freed) return; 
+            std::cout << "Free called!" << std::endl;
+            static_cast<D_*>(this)->free_impl();
+            freed = true; 
+        }
     };
     
-    struct string; 
 
     enum class SizeTypes : size_t {
         Byte     = 1,
@@ -67,7 +137,7 @@ namespace AutomaticMemory {
     /*
         Global heap class. 
         Memory is handled in this way;
-        We have a vector (or list) of segments. A segment is a individual memory 
+        We have a vector (or list) of segments. A segment is an individual memory 
         space lives parallel to other memory spaces (segments).
         Eg: 
         Vector<Segment>::Begin()---Segment------Segment------Segment------Segment---Vector<Segment>::End()
@@ -75,16 +145,17 @@ namespace AutomaticMemory {
                                     Raw mem     Raw mem      Raw mem      Raw mem
                                     sizeof(32)  sizeof(1024) sizeof(16)   sizeof(1024^2)
                                     bytes       bytes        bytes        bytes
-        As the diagram shows, they live parallel to each other so no memory collision or overlap.
-        The main memory management is provided by standard library since the Segment is a fancy
-        wrapper for std::vector<char> and Heap is a fancy wrapper and manager for std::vector<Segment>.
+
+        As the diagram shows, they live parallel to each other so no memory collision or overlap. (Except where memory 
+        overflow, check compiler specifications) The main memory management is provided by standard library since the Segment 
+        is a fancy wrapper for std::vector<unsigned char> and Heap is a fancy wrapper and manager for std::vector<Segment>.
         So it's as much memory safe as std::vector.   
     */
     class Heap {
     private:
         /* Segment
             This class represents a memory segment. Each segment is a memory space.
-            std::vector<char> is the holder for raw memory. 
+            std::vector<unsigned char> is the holder for raw memory. 
         */
         struct Segment {
             Segment() : size(0) {}
@@ -94,13 +165,13 @@ namespace AutomaticMemory {
             bool operator==(Segment const& other) const { return m_Memory == other.m_Memory and size == other.size; }
             auto begin() { return m_Memory.begin(); }
             auto end() { return m_Memory.end(); }
-            std::vector<char> m_Memory;
+            std::vector<unsigned char> m_Memory;
             size_t size;
         };
         /* 
             Low level allocation.
             We create a segment in the segments vector with a provided size.  As the segment gets constructed,
-            it reserves the requested memory size using std::vector<char>::reserve();
+            it reserves the requested memory size using std::vector<unsigned char>::reserve();
             Returns reference to allocated segment. 
         */
         Segment& allocate(size_t size) {
@@ -130,22 +201,46 @@ namespace AutomaticMemory {
             using base_type = base_pointer<T_, Pointer<T_, array>>;
             
             Pointer() = delete;
-            Pointer(Pointer&& other) : memsegment(other.memsegment), owner(std::move(other.owner)), base_type{std::move(other.m_Ptr), std::move(other.freed)} { other.freed = true; }
+            Pointer(Pointer const& other) = delete;
+            Pointer(Pointer&& other) : memsegment(other.memsegment), owner(std::move(other.owner)), base_type{std::move(other.m_Ptr), std::move(other.freed)} { other.moved = true; other.error.dont_exit(); }
 
             template<bool _array = array>
             typename std::enable_if<_array, T_&>::type operator[](size_t index) {
+                if (index < 0 or index > array_size) {
+                     
+                }
                 return *(base_type::m_Ptr + index); 
             }
+
+            Errors::base_error& Error() {
+                return error;
+            }
+
             private:
             friend class base_pointer<T_, Pointer>;
             friend class Heap; 
-
-            Pointer(T_ * pointer, Heap * owner, Segment& memsegment) : base_type{pointer}, owner{owner}, memsegment(memsegment) { }
+            
+            Pointer(T_ * pointer, Heap * owner, Segment& memsegment) : base_type{pointer}, owner{owner}, memsegment(memsegment) {}
 
             Segment& memsegment;
             Heap * owner;
+            Errors::base_error error;
+            size_t array_size; 
+            bool moved;
+
+            template<bool _array = array>
+            std::enable_if_t<_array, Pointer> SetSize(size_t size) {
+                array_size = size;
+                return *this;
+            } 
+
+            Pointer& SetError(Errors::base_error&& error) {
+                error = std::move(error);
+                return *this;
+            }
 
             void free_impl() {
+                if (moved) { return; }
                 if constexpr (array) {
                     for (size_t i = 0; i < memsegment.size / sizeof(T_); ++i) {
                         (base_type::m_Ptr + i)->~T_();
@@ -155,41 +250,65 @@ namespace AutomaticMemory {
                 }
                 owner->free(*this);
             }
+
         }; 
         
-        Heap() = default; 
+        Heap() { setatexit(); }; 
 
-        /* 
-            Allocates an object on the memory.
-            Allocation gets the size of type then reserves the exact size on the memory. When reserving is complete,
-            reserved Segment will be allocated with default constructor. If default constructor is not available,
-            construction will be skipped.
-        */
-        template<typename T_>
-        Pointer<T_, false> allocate() {
-            Segment& allocated = allocate(sizeof(T_)); 
-            T_ * f_Ptr = static_cast<T_*>(static_cast<void*>(allocated.m_Memory.data()));
-            if constexpr (std::is_default_constructible_v<T_>) {
-                new(f_Ptr) T_{}; 
-            }
-            return Pointer<T_, false>{f_Ptr, this, allocated}; 
-        }
         /*
             Allocates x amount of objects on the memory.
             Allocation gets the size of type and multiplies it with count of objects, then reserves the exact size on the memory. 
             When reserving is complete, reserved Segment will be allocated with default constructor of the type. If default constructor
-            is not available, constructio will be skipped.
+            is not available or no constructor parameters are supplied, build will fail.
         */
-        template<typename T_>
-        Pointer<T_, true> allocate(size_t count) {
+        template<typename T_, typename... ConstructorArgs>
+        Pointer<T_, true> allocate_constructed_n(size_t count, ConstructorArgs&&... args) {
             Segment& allocated = allocate(sizeof(T_) * count); 
-            T_ * f_Ptr = static_cast<T_*>(static_cast<void*>(allocated.m_Memory.data())); 
-            if constexpr (std::is_default_constructible_v<T_>) {
-                for(int i = 0; i < count; i++) {
-                    new(f_Ptr + i) T_();
+            T_ * f_Ptr = static_cast<T_*>(static_cast<void*>(allocated.m_Memory.data()));
+            try {
+                if constexpr (sizeof...(ConstructorArgs) > 0) {
+                    for (int i = 0; i < count; i++) {
+                        new(f_Ptr + i) T_(std::forward<ConstructorArgs>(args)...); 
+                    }
+                }
+                else if constexpr (std::is_default_constructible_v<T_>) {
+                    for(int i = 0; i < count; i++) {
+                        new(f_Ptr + i) T_{};
+                    }
+                } else {
+                    static_assert(false, "If type is not default constructible, you have to give constructor parameters!");
                 }
             }
-            return Pointer<T_, true>{f_Ptr, this, allocated}; 
+            catch(std::exception const& e) {
+                return Pointer<T_, true>{f_Ptr, this, allocated}.SetSize(count).SetError(std::move(Errors::BadConstruct{"Exception while constructing, construction stopped!\n  What: " + std::string(e.what())}));
+            }
+
+            return Pointer<T_, true>{f_Ptr, this, allocated}.SetSize(count); 
+        }
+
+        /* 
+            Allocates an object on the memory.
+            Allocation gets the size of type then reserves the exact size on the memory. When reserving is complete,
+            reserved Segment will be allocated with default constructor.  If default constructor is not available or 
+            no constructor parameters are supplied, build will fail.
+        */
+        template<typename T_, typename... ConstructorArgs>
+        Pointer<T_, false> allocate_constructed(ConstructorArgs&&... args) {
+            Segment& allocated = allocate(sizeof(T_)); 
+            T_ * f_Ptr = static_cast<T_*>(static_cast<void*>(allocated.m_Memory.data()));
+            try {
+                if constexpr (sizeof...(ConstructorArgs) > 0) {
+                    new(f_Ptr) T_{std::forward<ConstructorArgs>(args)...}; 
+                }
+                else if constexpr (std::is_default_constructible_v<T_>) {
+                    new(f_Ptr) T_{}; 
+                } else {
+                    static_assert(false, "If type is not default constructible, you have to give constructor parameters!");
+                }
+            } catch(std::exception const& e) {
+                return std::move(Pointer<T_, false>{f_Ptr, this, allocated}.SetError(std::move(Errors::BadConstruct{"Exception while constructing, construction stopped!\n  What: " + std::string(e.what())})));
+            }
+            return std::move(Pointer<T_, false>{f_Ptr, this, allocated}); 
         }
         /*
             Returns the estimated used memory. 
@@ -214,8 +333,8 @@ namespace AutomaticMemory {
             m_Segments.shrink_to_fit();
         }
         /*
-            Internal free method used by string class. 
-            When a string is ready to die, this method is called to release resources. Releases memory immediately.  
+            Internal free method reserved for further implementations on different data structures
+            that this class can be used as the allocator. 
         */
         void free(Segment& segment) {
             memory_in_use -= segment.size;
@@ -224,72 +343,21 @@ namespace AutomaticMemory {
             m_Segments.erase(it);
             m_Segments.shrink_to_fit();
         }
-        friend class string; 
+
+        void free_all() {
+            memory_in_use = 0; 
+            m_Segments.clear();
+            m_Segments.shrink_to_fit();
+        }
+
+        void setatexit(); 
     };
 
-    inline Heap heap; 
-    // Incomplete string type. WIP 
-    struct string {
-        public: 
-        string() : m_data(heap.allocate(0)) { } ; 
-        string(const char * str) : m_data(heap.allocate(strlen(str))) {
-            assign(str, strlen(str)); 
-        }
-        string(const std::string& str) : m_data(heap.allocate(str.length())) {
-            assign(str.data(), str.length()); 
-        }
-        string& operator=(const char * str) {
-            m_data.m_Memory.clear();
-            assign(str, strlen(str));
-            return *this;
-        }
-        string& operator=(std::string const& str) {
-            m_data.m_Memory.clear();
-            assign(str.data(), str.length()); 
-            return *this; 
-        }
-        void resize(size_t const& n_size) {
-            m_data.resize(n_size); 
-        }
-        void reserve(size_t const& n_size) {
-            m_data.reserve(n_size);
-        }
-        void append(char ch) {
-            m_data.m_Memory.insert(m_data.end(), ch); 
-        }
-        char at(size_t index) {
-            return m_data.m_Memory[index];
-        }
-        char * data() {
-            return m_data.m_Memory.data();
-        } 
-        const char * c_str() {
-            return m_data.m_Memory.data();
-        }
-        void clear() {
-            return m_data.m_Memory.clear();
-        }
-        void free() {
-            if (freed) { return; }
-            freed = true; 
-            heap.free(m_data);
-        }
-        operator const char *() {
-            return data(); 
-        }
-        operator std::string() {
-            return {data()};
-        }
-        ~string() {
-            free();
-        }
-        private: 
-        void assign(const char * str, size_t len) {
-            m_data.m_Memory.reserve(len + 1);
-            std::copy(str, str + len, std::back_inserter(m_data.m_Memory));
-            m_data.m_Memory.push_back(0);
-        }
-        Heap::Segment& m_data;
-        bool freed = false;
-    }; 
+    inline Heap heap;
+
+    inline void Heap::setatexit() {
+        std::atexit([] () {
+            heap.free_all();           
+        }); 
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "MemManage.hpp"
 #include <iostream>
 #include <cstdio>
+#include <stdexcept>
 
 using AutomaticMemory::heap; 
 
 struct A {
     A() : str{"Hello World!"} { }
+    A(std::string const& str) : str(str) {
+        throw std::runtime_error("some error");
+    }
     std::string str; 
 };
 
 auto test() {
-    auto x = heap.allocate<A>(1024 * 1024 * 10);
+    auto x = heap.allocate_constructed<A>("Hi World!");
+    
+    x.Error().dont_exit();
+
     std::cout << "Press enter to move out pointer." << std::endl;
     getchar();
     return std::move(x);
@@ -20,7 +27,6 @@ auto foo() {
     auto x = test();
     std::cout << x->str << std::endl; // validate pointer still lives.
     std::cout << "Pointer now belongs to foo(). When foo ends, pointer will die. Press enter to kill pointer." << std::endl;
-    
     getchar();
 } // now the allocated space is released.
 
